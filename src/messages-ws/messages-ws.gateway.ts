a8d7755f65ea5846b39a14d37f021ae32ef4ac08
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2022-08-06 06:05:11 -0500

    fin sección 15

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,73 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'http';
+import { Socket } from 'socket.io';
+import { MessagesWsService } from './messages-ws.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() wss: Server;
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+    // console.log('Cliente conectado: ', client.id);
+    // console.log({ conectados: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  handleDisconnect(client: Socket) {
+    // console.log('Cliente desconectado: ', client.id);
+    this.messagesWsService.removeClient(client.id);
+    // console.log({ conectados: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: any) {
+    //! Emite únicamente al cliente. No a todos los clientes.
+    // client.emit('message-from-server', {
+    //   fullName: 'Soy Yo',
+    //   message: payload.message || 'no-message!!',
+    // });
+
+    //! Emitr a todos MENOS al cliente.
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy Yo',
+    //   message: payload.message || 'no-message!!',
+    // });
+
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message!!',
+    });
+  }
+}
