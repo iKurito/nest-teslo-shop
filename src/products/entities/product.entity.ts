bab1c8ff613eacab89e5c397f56ea954655484d5
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2022-08-06 04:34:18 -0500

    fin sección 14

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,46 +9,87 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { User } from '../../auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '5e8f8f8f-8f8f-8f8f-8f8f-8f8f8f8f8f8f',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'T-shirt Teslo',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Product Price',
+  })
   @Column('float', {
     default: 0,
   })
   price: number;
 
+  @ApiProperty({
+    example:
+      'Ad consectetur occaecat minim magna qui dolore fugiat sint magna. ',
+    description: 'Product Description',
+    default: null,
+  })
+  @ApiProperty()
   @Column({
     type: 'text',
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 't_shirt_teslo',
+    description: 'Product SLUG - for SEO',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['M', 'XL', 'XXL'],
+    description: 'Product Sizes',
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'women',
+    description: 'Product Gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty()
   @Column('text', {
     array: true,
     default: [],
@@ -56,6 +97,7 @@ export class Product {
   tags: string[];
 
   // images
+  @ApiProperty()
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -15,13 +15,23 @@ import { PaginationDto } from '../common/dto/pagination.dto';
 import { Auth, GetUser } from '../auth/decorators';
 import { ValidRoles } from '../auth/interfaces';
 import { User } from '../auth/entities/user.entity';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Product } from './entities/product.entity';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
   @Auth()
+  @ApiResponse({
+    status: 201,
+    description: 'Product was created',
+    type: Product,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 403, description: 'Forbidden. Token related.' })
   create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
     return this.productsService.create(createProductDto, user);
   }
